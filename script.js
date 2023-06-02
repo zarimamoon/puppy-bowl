332c178f2c65ea91ede6f61e47ec9c34b2fb5dd1
Dana <126463047+hey-dane@users.noreply.github.com>
2023-06-01 18:01:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const playerContainer = document.getElementById('all-players-container');
 const newPlayerFormContainer = document.getElementById('new-player-form');
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = '2302-ACC-PT-WEB-PT-C';
+const cohortName = "2302-ACC-PT-WEB-PT-C";
 ';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
