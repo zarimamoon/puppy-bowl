6c1d23d92e41d25695d60ba171209a5b2ae46297
Dana <126463047+hey-dane@users.noreply.github.com>
2023-06-02 20:18:51 -0700

    updated css styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const playerContainer = document.getElementById("all-players-container");
 const newPlayerFormContainer = document.getElementById("new-player-form");
+const bodyDiv = document.querySelector("body");
 
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
 const cohortName = "2302-ACC-PT-WEB-PT-C";
@@ -15,7 +16,7 @@ const fetchAllPlayers = async () => {
     const response = await fetch(`${APIURL}/players`);
     const result = await response.json();
     const allPlayers = result.data.players;
-    console.log(allPlayers);
+    console.log(result);
     return allPlayers;
   } catch (err) {
     console.error("Uh oh, trouble fetching players!", err);
@@ -26,6 +27,7 @@ const fetchSinglePlayer = async (playerId) => {
   try {
     const response = await fetch(`${APIURL}/players/${playerId}`);
     const singlePlayer = await response.json();
+    console.log(singlePlayer);
     return singlePlayer;
   } catch (err) {
     console.error(`Oh no, trouble fetching player #${playerId}!`, err);
@@ -64,7 +66,7 @@ const removePlayer = async (playerId) => {
   }
 };
 
-/**
+/*
  * It takes an array of player objects, loops through them, and creates a string of HTML for each
  * player, then adds that string to a larger string of HTML that represents all the players.
  *
@@ -84,47 +86,49 @@ const removePlayer = async (playerId) => {
  * @param playerList - an array of player objects
  * @returns the playerContainerHTML variable.
  */
+
+// Create single player cards
 const renderAllPlayers = async (playerList, playerContainer) => {
   try {
-    // Clear the content of playerContainer before rendering
-    playerContainer.innerHTML = "";
-
+    // create a header for All Players
     const allH1 = document.createElement("h1");
-    allH1.innerHTML = "All Players";
+    allH1.innerHTML = "All Puppies";
     playerContainer.appendChild(allH1);
 
-    playerContainer.style.background = "beige";
-    playerContainer.style.display = "grid";
-    playerContainer.style.height = "600px";
-    playerContainer.style.width = "600px";
-    playerContainer.style.justifyContent = "center";
-    playerContainer.style.alignContent = "center";
-
+    //loop through player objects
     playerList.forEach((player) => {
-      const playerElement = document.createElement("div");
-      playerElement.classList.add("player");
+      const playerCard = document.createElement("div");
+      playerCard.classList.add("card");
 
       const nameElement = document.createElement("h2");
       nameElement.textContent = player.name;
-      playerElement.appendChild(nameElement);
+      playerCard.appendChild(nameElement);
 
-      const breedElement = document.createElement("p");
-      breedElement.textContent = player.breed;
-      playerElement.appendChild(breedElement);
+      const buttonContainer = document.createElement("div");
+      buttonContainer.classList.add("buttons");
+      playerCard.appendChild(buttonContainer);
 
-      const statusElement = document.createElement("p");
-      statusElement.textContent = player.status;
-      playerElement.appendChild(statusElement);
+      // Create the "See Details" button
+      const detailsButton = document.createElement("button");
+      detailsButton.classList.add("details-button");
+      detailsButton.setAttribute("data-id", player.id);
+      detailsButton.textContent = "See Details";
+      buttonContainer.appendChild(detailsButton);
 
-      const imageUrlElement = document.createElement("p");
-      imageUrlElement.textContent = player.imageUrl;
-      playerElement.appendChild(imageUrlElement);
+      // Create the "Delete" button
+      const deleteButton = document.createElement("button");
+      deleteButton.classList.add("delete-button");
+      deleteButton.setAttribute("data-id", player.id);
+      deleteButton.textContent = "Delete";
+      buttonContainer.appendChild(deleteButton);
 
-      const teamIdElement = document.createElement("p");
-      teamIdElement.textContent = player.teamId;
-      playerElement.appendChild(teamIdElement);
+      playerContainer.appendChild(playerCard);
 
-      playerContainer.appendChild(playerElement);
+      // Add Event listener for "See details" button
+
+      // Add add Event listener for "Remove from roster" button
+
+      // After deleting the party, re-render all parties
     });
   } catch (err) {
     console.error("Uh oh, trouble rendering players!", err);
@@ -150,7 +154,7 @@ const renderNewPlayerForm = async () => {
   puppyNameInput.setAttribute("id", "puppyValue");
   puppyNameInput.setAttribute("type", "text");
   puppyNameInput.setAttribute("name", "puppyName");
-  puppyNameInput.setAttribute("placeholder", "Puppy Name");
+  puppyNameInput.setAttribute("placeholder", "Puppy's Name");
   form.appendChild(puppyNameInput);
 
   // Create an input element for breed
@@ -180,52 +184,7 @@ const renderNewPlayerForm = async () => {
   // Append the form to the newPlayerFormContainer element
   newPlayerFormContainer.appendChild(form);
 
-  // Apply styles to the form
-  form.style.background = "#c2c6f9";
-  form.style.display = "grid";
-  form.style.height = "600px";
-  form.style.width = "600px";
-  form.style.justifyContent = "center";
-  form.style.alignContent = "center";
-  form.style.marginTop = "100px";
-
-  puppyNameInput.style.padding = "10px";
-  puppyNameInput.style.margin = "10px";
-  dogBreedInput.style.padding = "10px";
-  dogBreedInput.style.margin = "10px";
-  imgURL.style.padding = "10px";
-  imgURL.style.margin = "10px";
-
-  formH1.style.letterSpacing = "3px";
-  formH1.style.width = "500px";
-
-  function applyFontToElements() {
-    var elements = document.querySelectorAll("body");
-    elements.forEach(function (element) {
-      element.style.fontFamily = "Montserrat, sans-serif";
-      element.style.wordSpacing = "23px";
-      element.style.display = "grid";
-      element.style.justifyItems = "center";
-    });
-  }
-
-  // Load the font and apply it to the elements after it has finished loading
-  var link = document.createElement("link");
-  link.href =
-    "https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap";
-  link.rel = "stylesheet";
-  document.head.appendChild(link);
-
-  link.addEventListener("load", applyFontToElements);
-
-  const playerObj = {
-    name: puppyNameInput.value,
-    breed: dogBreedInput.value,
-    imageURL: imgURL.value,
-    status: "active",
-    teamID: null,
-  };
-
+  // add event listener to submit button
   form.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -240,6 +199,8 @@ const renderNewPlayerForm = async () => {
       imageUrl: playerImageUrl,
       teamId: null,
     };
+
+    console.log(newPlayer);
 
     await addNewPlayer(newPlayer);
 
